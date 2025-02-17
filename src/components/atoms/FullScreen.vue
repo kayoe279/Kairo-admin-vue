a6345e23f9fb13a6bcf70e088a9e17107afdeebb
Kay <kay@rb2.nl>
2025-02-17 14:35:41 +0800

    重构 tabs

--- src/components/atoms/FullScreen.vue
+++ src/components/atoms/FullScreen.vue
@@ -11,7 +11,7 @@ defineProps<Props>();
 </script>
 
 <template>
-  <ButtonIcon :tooltip-content="$t('icon.reload')">
+  <ButtonIcon tooltip-content="刷新页面">
     <icon-ant-design-reload-outlined :class="{ 'animate-duration-750 animate-spin': loading }" />
   </ButtonIcon>
 </template>
